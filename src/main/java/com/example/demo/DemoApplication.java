06b172911a82309ebae30ff58c68f82bab088d3d
pgy <2573426+pengliaoye@users.noreply.github.com>
2025-11-06 19:12:46 +0800

    e

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import io.awspring.cloud.sqs.annotation.SqsListener;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -11,6 +12,11 @@ public class DemoApplication {
 	@Bean
 	public RestTemplate restTemplate() {
 		return new RestTemplate();
+	}
+
+	@SqsListener("zaobao-com.fifo")
+	public void listen(String message) {
+		System.out.println(message);
 	}
 
 	public static void main(String[] args) {
